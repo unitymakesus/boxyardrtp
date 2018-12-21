0d6885ee5d91fb834fef69295da4bc9f1b26086d
lexinamer <lexinamer@gmail.com>
2018-12-21 14:51:03 -0600

    adding plugins

---
+++ wp-content/plugins/bb-plugin/includes/updater-config.php
@@ -0,0 +1,10 @@
+<?php
+
+if ( class_exists( 'FLUpdater' ) ) {
+	FLUpdater::add_product(array(
+		'name'      => 'Beaver Builder Plugin (Standard Version)',
+		'version'   => '2.1.7.1',
+		'slug'      => 'bb-plugin',
+		'type'      => 'plugin',
+	));
+}
