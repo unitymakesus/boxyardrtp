e24e4ed0af4a67b259ce9a28471b0a679a92dd68
Alisa Herr <alisa@unitymakes.us>
2018-12-28 11:25:37 -0500

    Remove unneeded PHP files from child theme

--- wp-content/themes/boxyardrtp/resources/functions.php
+++ wp-content/themes/boxyardrtp/resources/functions.php
@@ -1,92 +1,47 @@
 <?php
 
-/**
- * Do not edit anything in this file unless you know what you're doing
- */
-
-use Roots\Sage\Config;
-use Roots\Sage\Container;
+namespace App;
 
 /**
- * Helper function for prettying up errors
- * @param string $message
- * @param string $subtitle
- * @param string $title
+ * Theme assets
  */
-$sage_error = function ($message, $subtitle = '', $title = '') {
-    $title = $title ?: __('Sage &rsaquo; Error', 'sage');
-    $footer = '<a href="https://roots.io/sage/docs/">roots.io/sage/docs/</a>';
-    $message = "<h1>{$title}<br><small>{$subtitle}</small></h1><p>{$message}</p><p>{$footer}</p>";
-    wp_die($message, $title);
-};
+add_action('wp_enqueue_scripts', function () {
+  // Unenqueue files from parent theme
+  wp_dequeue_style('sage/main.css');
+  wp_dequeue_script('sage/main.js');
+
+  // Enqueue files for child theme (which include the above as imports)
+  wp_enqueue_style('sage/main.css', asset_path('styles/main.css'), false, null);
+  wp_enqueue_script('sage/main.js', asset_path('scripts/main.js'), ['jquery'], null, true);
+
+  // Set array of theme customizations for JS
+  wp_localize_script( 'sage/main.js', 'simple_options', array('fonts' => get_theme_mod('theme_fonts'), 'colors' => get_theme_mod('theme_color')) );
+}, 100);
 
 /**
- * Ensure compatible version of PHP is used
+ * Register sidebars
  */
-if (version_compare('7.1', phpversion(), '>=')) {
-    $sage_error(__('You must be using PHP 7.1 or greater.', 'sage'), __('Invalid PHP version', 'sage'));
-}
-
-/**
- * Ensure compatible version of WordPress is used
- */
-if (version_compare('4.7.0', get_bloginfo('version'), '>=')) {
-    $sage_error(__('You must be using WordPress 4.7.0 or greater.', 'sage'), __('Invalid WordPress version', 'sage'));
-}
-
-/**
- * Ensure dependencies are loaded
- */
-if (!class_exists('Roots\\Sage\\Container')) {
-    if (!file_exists($composer = __DIR__.'/../vendor/autoload.php')) {
-        $sage_error(
-            __('You must run <code>composer install</code> from the Sage directory.', 'sage'),
-            __('Autoloader not found.', 'sage')
-        );
-    }
-    require_once $composer;
-}
-
-/**
- * Sage required files
- *
- * The mapped array determines the code library included in your theme.
- * Add or remove files to the array as needed. Supports child theme overrides.
- */
-array_map(function ($file) use ($sage_error) {
-    $file = "../app/{$file}.php";
-    if (!locate_template($file, true, true)) {
-        $sage_error(sprintf(__('Error locating <code>%s</code> for inclusion.', 'sage'), $file), 'File not found');
-    }
-}, ['helpers', 'setup']);
-
-/**
- * Here's what's happening with these hooks:
- * 1. WordPress initially detects theme in themes/sage/resources
- * 2. Upon activation, we tell WordPress that the theme is actually in themes/sage/resources/views
- * 3. When we call get_template_directory() or get_template_directory_uri(), we point it back to themes/sage/resources
- *
- * We do this so that the Template Hierarchy will look in themes/sage/resources/views for core WordPress themes
- * But functions.php, style.css, and index.php are all still located in themes/sage/resources
- *
- * This is not compatible with the WordPress Customizer theme preview prior to theme activation
- *
- * get_template_directory()   -> /srv/www/example.com/current/web/app/themes/sage/resources
- * get_stylesheet_directory() -> /srv/www/example.com/current/web/app/themes/sage/resources
- * locate_template()
- * ├── STYLESHEETPATH         -> /srv/www/example.com/current/web/app/themes/sage/resources/views
- * └── TEMPLATEPATH           -> /srv/www/example.com/current/web/app/themes/sage/resources
- */
-array_map(
-    'add_filter',
-    ['theme_file_path', 'theme_file_uri', 'parent_theme_file_path', 'parent_theme_file_uri'],
-    array_fill(0, 4, 'dirname')
-);
-Container::getInstance()
-    ->bindIf('config', function () {
-        return new Config([
-            'assets' => require dirname(__DIR__).'/config/assets.php',
-            'theme' => require dirname(__DIR__).'/config/theme.php',
-            'view' => require dirname(__DIR__).'/config/view.php',
-        ]);
-    }, true);
+add_action('widgets_init', function () {
+  $config = [
+    'before_widget' => '<section class="widget %1$s %2$s">',
+    'after_widget'  => '</section>',
+    'before_title'  => '<h3>',
+    'after_title'   => '</h3>'
+  ];
+  register_sidebar([
+    'name'          => __('Footer-Social-Left', 'sage'),
+    'id'            => 'footer-social-left'
+  ] + $config);
+  register_sidebar([
+    'name'          => __('Footer-Social-Right', 'sage'),
+    'id'            => 'footer-social-right'
+  ] + $config);
+  register_sidebar([
+    'name'          => __('Footer-Utility-Left', 'sage'),
+    'id'            => 'footer-utility-left'
+  ] + $config);
+  register_sidebar([
+    'name'          => __('Footer-Utility-Right', 'sage'),
+    'id'            => 'footer-utility-right'
+  ] + $config);
+});
